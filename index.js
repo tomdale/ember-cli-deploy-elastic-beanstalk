ca29eb489ccb4f607909e63865beab9d3e698951
Tom Dale <tom@tomdale.net>
2016-01-04 16:07:46 -0500

    Adds provisioning command

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 'use strict';
 
 var ElasticBeanstalkDeployPlugin = require('./lib/elastic-beanstalk-deploy-plugin');
+var ProvisionCommand = require('./lib/commands/provision');
 
 module.exports = {
   name: 'ember-cli-deploy-elastic-beanstalk',
+
+  includedCommands: function() {
+    return { 'eb:provision': ProvisionCommand };
+  },
 
   createDeployPlugin: function(options) {
     return new ElasticBeanstalkDeployPlugin({
